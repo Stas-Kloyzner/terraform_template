55d6e8a4f6dc29d054d367ce9bf5151b35b82145
Stas-Kloyzner <92732559+Stas-Kloyzner@users.noreply.github.com>
2022-06-29 14:19:25 +0300

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,17 @@
+#root module backend
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name = "storage"
+    storage_account_name = "w5tfstate"
+    container_name = "tfstate"
+  }
+
+  required_version = ">= 1.1.0"
+}
